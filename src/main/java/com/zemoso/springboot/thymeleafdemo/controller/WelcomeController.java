c14b53fe1753d08a9accbfc2b447e44601d57ab4
Srikanth854 <shreekanth854@gmail.com>
2021-11-08 19:16:00 +0530

    Initial commit

---
+++ src/main/java/com/zemoso/springboot/thymeleafdemo/controller/WelcomeController.java
@@ -0,0 +1,35 @@
+package com.zemoso.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.zemoso.springboot.thymeleafdemo.dto.StudentDto;
+import com.zemoso.springboot.thymeleafdemo.service.StudentService;
+
+@Controller
+public class WelcomeController {
+
+    private StudentService studentService;
+
+
+    public WelcomeController(StudentService theStudentService) {
+        studentService=theStudentService;
+    }
+
+    @GetMapping("/")
+    public String listStudents(Model theModel)
+
+    {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        List<StudentDto> theStudents = studentService.findAll();
+
+        theModel.addAttribute("students",theStudents);
+
+        return "students/list-students";
+    }
+
+}
